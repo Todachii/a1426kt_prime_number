fd97158f9092a2b02e4b202920bc503e25dccb10
kikuko toda <k-toda@gmail.com>
2014-11-03 02:21:04 +0000

    version up

--- lib/a1426kt_prime_number.rb
+++ lib/a1426kt_prime_number.rb
@@ -1,5 +1,24 @@
 require "a1426kt_prime_number/version"
 
-module A1426ktPrimeNumber
+class A1426ktPrimeNumber
+#module A1426ktPrimeNumber
   # Your code goes here...
+  def determine_prime(num)
+    if num == 1 || num == 2 then
+      return "This is the prime number."
+    end
+    if num % 2 == 0 then
+      return "This is not the prime number."
+    end
+    i = 3
+    while true
+      if num == i then
+        return "This is the prime number."
+      end
+      if num % i == 0 then
+        return "This is not the prime number."
+      end
+      i = i + 2
+    end
+  end
 end
