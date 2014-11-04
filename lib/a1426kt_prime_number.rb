c22efe2b0c46eaac931db11bfd62bc5845ab34dd
kikuko toda <k-toda@gmail.com>
2014-11-04 11:45:12 +0000

    test

--- lib/a1426kt_prime_number.rb
+++ lib/a1426kt_prime_number.rb
@@ -1,15 +1,19 @@
-#require "a1426kt_prime_number/version"
+require "a1426kt_prime_number/version"
 
-module A1426ktPrimeNumber
+class A1426kt_prime_number
   # Your code goes here...
   def determine_prime(num)
+
     if num == 1 || num == 2 then
       return "This is the prime number."
     end
+
     if num % 2 == 0 then
       return "This is not the prime number."
     end
+
     i = 3
+
     while true
       if num == i then
         return "This is the prime number."
@@ -19,6 +23,8 @@ module A1426ktPrimeNumber
       end
       i = i + 2
     end
+
   end
+
 end
 
