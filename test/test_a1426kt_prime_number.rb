047f243a7932342ad596f3010f28ff194966bbb6
kikuko toda <k-toda@gmail.com>
2014-11-04 12:04:54 +0000

    finish

--- test/test_a1426kt_prime_number.rb
+++ test/test_a1426kt_prime_number.rb
@@ -3,8 +3,14 @@ require 'a1426kt_prime_number'
 
 class A1426ktPrimeNumberTest < MiniTest::Unit::TestCase
   def test_determine_prime
-    assert_equal "This is the prime number",
-      A1426ktPrimeNumber.determine_prime(3)
+    obj = A1426kt_prime_number.new
+    assert_equal "This is the prime number.",
+      obj.determine_prime(191)
+  end
+  def test_not_determine_prime
+    obj = A1426kt_prime_number.new
+    assert_equal "This is not the prime number.",
+      obj.determine_prime(100)
   end
 
 end
